0cbe53a1c8031112af921fbded66f5cc8efe00e2
mustafa616172-lgtm <mustafa616172@gmail.com>
2026-02-02 23:10:18 +0300

    menü bağlandı

---
+++ Assets/MainMenu/MainMenuController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Button References")]
+    public Button startGameButton;
+    public Button quitButton;
+    
+    [Header("Game Settings")]
+    public string gameSceneName = "Echoes";
+
+    void Start()
+    {
+        if (startGameButton != null)
+        {
+            startGameButton.onClick.AddListener(OnStartGameClicked);
+            Debug.Log("[MainMenu] Start Game button connected");
+        }
+
+        if (quitButton != null)
+        {
+            quitButton.onClick.AddListener(OnQuitClicked);
+            Debug.Log("[MainMenu] Quit button connected");
+        }
+    }
+
+    public void OnStartGameClicked()
+    {
+        Debug.Log("[MainMenu] Starting game: " + gameSceneName);
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OnQuitClicked()
+    {
+        Debug.Log("[MainMenu] Quitting game...");
+        
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
