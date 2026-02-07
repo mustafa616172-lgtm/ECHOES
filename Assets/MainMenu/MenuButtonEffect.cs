70608fb9c815692441372405d5e75f19f1904cfc
bedirksknn <boynuyorsa@gmail.com>
2026-02-07 11:56:29 +0300

    ana menü

--- Assets/MainMenu/MenuButtonEffect.cs
+++ Assets/MainMenu/MenuButtonEffect.cs
@@ -1,82 +1,109 @@
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems; // Mouse olaylarýný algýlamak için gerekli
-using TMPro; // TextMeshPro kullanýyorsan gerekli
+ï»¿using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class MenuButtonEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class MenuButtonEffect : MonoBehaviour,
+    IPointerEnterHandler,
+    IPointerExitHandler,
+    IPointerClickHandler
 {
-    [Header("Görsel Ayarlar")]
-    public TextMeshProUGUI buttonText; // Eðer normal Text kullanýyorsan burayý 'Text' olarak deðiþtir
-    public Color normalColor = Color.gray; // Normal hali (Örn: Gri)
-    public Color hoverColor = Color.white; // Üzerine gelinceki renk (Örn: Beyaz/Parlak)
-    
-    [Range(1f, 1.5f)]
-    public float hoverScale = 1.1f; // Üzerine gelince ne kadar büyüsün? (1.1 = %10 büyüme)
-    public bool enableGlowEffect = true; // Materyal glow'unu açýp kapatmak ister misin?
+    [Header("YazÄ± AyarlarÄ±")]
+    public TextMeshProUGUI buttonText;
 
-    [Header("Ses Ayarlarý")]
-    public AudioSource audioSource; // Sesin çýkacaðý kaynak
-    public AudioClip hoverSound;    // Üzerine gelme sesi (bip, hýþýrtý vs.)
-    public AudioClip clickSound;    // Týklama sesi
+    public Color normalColor = Color.gray;
+    public Color hoverColor = Color.white;
+
+    [Range(1f, 1.5f)]
+    public float hoverScale = 1.1f;
+
+    public bool enableGlowEffect = true;
+
+    [Header("Glow AyarlarÄ±")]
+    public Color glowColor = Color.white;
+    [Range(0f, 1f)]
+    public float glowOuter = 0.5f;
+    [Range(0f, 2f)]
+    public float glowPower = 1.2f;
+
+    [Header("Ses AyarlarÄ±")]
+    public AudioSource audioSource;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
 
     private Vector3 initialScale;
+    private Material textMaterial;
 
     void Start()
     {
-        // Baþlangýç boyutunu ve rengini kaydet
-        if (buttonText == null) 
+        if (buttonText == null)
             buttonText = GetComponentInChildren<TextMeshProUGUI>();
 
         initialScale = transform.localScale;
-        
-        // Baþlangýçta normal renge döndür
-        if (buttonText != null)
-            buttonText.color = normalColor;
-            
-        // Eðer materyal glow kullanýyorsan baþlangýçta kapat
-        if (enableGlowEffect && buttonText != null)
-            buttonText.fontSharedMaterial.EnableKeyword(ShaderUtilities.Keyword_Glow);
+
+        // YazÄ± rengi
+        buttonText.color = normalColor;
+
+        // ðŸ”¥ MATERIAL INSTANCE (Ã‡OK Ã–NEMLÄ°)
+        textMaterial = new Material(buttonText.fontMaterial);
+        buttonText.fontMaterial = textMaterial;
+
+        // Glow baÅŸlangÄ±Ã§ta kapalÄ±
+        textMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 0f);
+        textMaterial.SetFloat(ShaderUtilities.ID_GlowOuter, 0f);
     }
 
-    // Mouse butonun üzerine geldiðinde çalýþýr
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // 1. Yazý Rengini Deðiþtir (Parlaklýk hissi)
-        if (buttonText != null)
-            buttonText.color = hoverColor;
+        // Renk deÄŸiÅŸimi
+        buttonText.color = hoverColor;
 
-        // 2. Butonu Hafifçe Büyüt
+        // BÃ¼yÃ¼me
         transform.localScale = initialScale * hoverScale;
 
-        // 3. (Opsiyonel) TMP Glow Efektini aç (Neon etkisi için)
-        if (enableGlowEffect && buttonText != null)
-            buttonText.fontMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 0.5f); // Parlaklýðý artýr
+        // Glow aÃ§
+        if (enableGlowEffect)
+        {
+            textMaterial.SetColor(
+                ShaderUtilities.ID_GlowColor,
+                glowColor
+            );
 
-        // 4. Sesi Çal
-        if (audioSource != null && hoverSound != null)
+            textMaterial.SetFloat(
+                ShaderUtilities.ID_GlowOuter,
+                glowOuter
+            );
+
+            textMaterial.SetFloat(
+                ShaderUtilities.ID_GlowPower,
+                glowPower
+            );
+        }
+
+        // Hover sesi
+        if (audioSource && hoverSound)
             audioSource.PlayOneShot(hoverSound);
     }
 
-    // Mouse butonun üzerinden gittiðinde çalýþýr
     public void OnPointerExit(PointerEventData eventData)
     {
-        // Her þeyi eski haline getir
-        if (buttonText != null)
-            buttonText.color = normalColor;
-
+        // Eski haline dÃ¶n
+        buttonText.color = normalColor;
         transform.localScale = initialScale;
-        
-        // Glow efektini sýfýrla
-        if (enableGlowEffect && buttonText != null)
-            buttonText.fontMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 0f);
+
+        // Glow kapat
+        if (enableGlowEffect)
+        {
+            textMaterial.SetFloat(
+                ShaderUtilities.ID_GlowPower,
+                0f
+            );
+        }
     }
 
-    // Butona týklandýðýnda çalýþýr
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (audioSource != null && clickSound != null)
+        // TÄ±klama sesi
+        if (audioSource && clickSound)
             audioSource.PlayOneShot(clickSound);
-            
-        // Burada oyun baþlatma kodu da çaðrýlabilir ama onu ayrý tutmak daha temizdir.
     }
 }
